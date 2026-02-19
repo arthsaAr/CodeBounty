3695e8708350e294d36c0f0da0a4b6f2cd4f3ada
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-19 14:08:56 -0700

    Adding some comments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+//express here creates the server/thing that listens on port 3000 and handles HTTP requests
+//responsinble for routes, endpoitns, REST APIs
 import express from "express";
 import dotenv from "dotenv";
 import {PrismaClient} from "@prisma/client";
@@ -13,6 +15,13 @@ dotenv.config();
 
 const prisma = new PrismaClient();
 const app = express();      //creating the web server
+
+//all these router parts are called REST APIs endpoints
+//it means, that it uses HTTP methods (GET, POST, PATCH, DELETE)
+//it also uses URLs to represent resources
+//and returns JSON
+//when using POSTMAN, it actually send the call REST API using POST/GET URL...
+
 app.use(express.json());        //this helps server to understand JSON data in request
 app.use(passport.initialize());
 app.use("/auth", authRoutes);
