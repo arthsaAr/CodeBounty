24a4605905e3fb00261f410bf21ff29454ac23fc
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-26 00:06:44 -0700

    fixing button placement

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function LoginPage() {
     return (
-        <div className="min-h-screen bg-black text-white flex items-center justify-center">
+        <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold">
                 <span className="text-green-400">&lt;/&gt;</span>{" "}
                 <span>Code</span>
                 <span className="text-green-400">Bounty</span>
             </h1>
             
-            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mt-3 rounded "
                 onClick={() => {
                     window.location.href = "http://localhost:3000/auth/github"
                 }}
