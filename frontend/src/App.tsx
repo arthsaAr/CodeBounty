f39f3b4b3f38d548bf27b817fcd3937cdb9e8297
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-26 00:05:17 -0700

    starting structuring login page along with backend functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
+import LoginPage from "./pages/LoginPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        <span className="text-green-400">&lt;/&gt;</span>{" "}
-        <span>Code</span>
-        <span className="text-green-400">Bounty</span>
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+    
+    // <div className="min-h-screen bg-black text-white flex items-center justify-center">
+    //   <h1 className="text-4xl font-bold">
+    //     <span className="text-green-400">&lt;/&gt;</span>{" "}
+    //     <span>Code</span>
+    //     <span className="text-green-400">Bounty</span>
+    //   </h1>
+    // </div>
   );
 }
 
