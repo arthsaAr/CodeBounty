b34d360b923b0fa16dd11f093e5fbd82a9bc7eeb
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-27 22:40:26 -0700

    Completed basic structure of login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { SiGithub } from "react-icons/si";
+import { FiGithub  } from "react-icons/fi";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { IoShieldOutline } from "react-icons/io5";
+import { FiTarget } from "react-icons/fi";
 
 export default function LoginPage() {
     return (
@@ -13,30 +16,44 @@ export default function LoginPage() {
                 Peer-to-peer bug bounty platform for GitHub repositories
             </h2>
             
-            <div className="bg-[#151920] p-4 rounded-sm w-96 text-center">
+            <div className="bg-[#151920] p-4 rounded-sm w-96 text-center mt-4">
                 <h2 className="text-lg font-semibold">Choose Your Role</h2>
 
-                <div className="bg-[#151920] py-8 rounded-lg mt-4 border-gray-700 hover:border-green-600 border-2">
-
+                <div className="bg-[#151920] py-5 rounded-lg mt-4 border-gray-700 hover:border-green-600 border-2 flex items-start gap-4">
+                    <FiTarget color="green" className="ml-1" size={30}/>
+                    <div className="flex flex-col text-left">
+                        <h2 className="font-semibold">Bug Hunter</h2>
+                        <h3 className="text-xs text-gray-500 font-semibold">Review code and submit bug reports for rewards</h3>
+                    </div>
                 </div>
 
-                <div className="bg-[#151920] py-8 rounded-lg mt-2 border-gray-700  hover:border-green-600 border-2">
-
+                <div className="bg-[#151920] py-5  rounded-lg mt-2 border-gray-700  hover:border-green-600 border-2 flex items-start gap-4">
+                    <IoShieldOutline color="green"  className="ml-1" size={30}/>
+                    <div className="flex flex-col text-left">
+                        <h2 className="font-semibold">Repository Owner</h2>
+                        <h3 className="text-xs text-gray-500 font-semibold">Post bounties and get your code reviewed</h3>
+                    </div>
                 </div>
 
-                <div className="bg-[#151920] py-8 rounded-lg mt-2 border-gray-700  hover:border-green-600 border-2">
-
+                <div className="bg-[#151920] py-5 rounded-lg mt-2 border-gray-700  hover:border-green-600 border-2 flex items-start gap-4">
+                    <FaArrowTrendUp color="green"  className="ml-1" size={30}/>
+                    <div className="flex flex-col text-left">
+                        <h2 className="font-semibold">Both</h2>
+                        <h3 className="text-xs text-gray-500 font-semibold">Hunt bugs and post bounties on your repositories</h3>
+                    </div>
+                    
                 </div>
 
 
 
-                <button className="bg-[#10b981] hover:bg-green-600 text-black font-semibold py-2 px-4 mt-3 rounded "
+                <button className="bg-[#10b981] hover:bg-green-600 text-black font-semibold py-2 mt-3 rounded w-full"
                                 onClick={() => {
                                     window.location.href = "http://localhost:3000/auth/github"
                                 }}
                             >
-                            <SiGithub className="mb-1 inline" />    Sign in with GitHub
+                            <FiGithub  className="mb-1 inline" />    Sign in with GitHub
                 </button>
+                <h3 className="text-xs mt-2 text-gray-400">By signing in, you agree to our Terms of Service</h3>
             </div>
 
             
