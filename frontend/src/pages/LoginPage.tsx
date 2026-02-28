d3dd8c7c737f6880d29adac7cc6fbe4ab99a23f5
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-27 21:17:26 -0700

    Starting work on login-page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,13 +3,16 @@ import React from "react";
 export default function LoginPage() {
     return (
         <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-4xl font-semibold">
                 <span className="text-green-400">&lt;/&gt;</span>{" "}
                 <span>Code</span>
                 <span className="text-green-400">Bounty</span>
             </h1>
+            <h2 className="mt-2 text-gray-400">
+                Peer-to-peer bug bounty platform for GitHub repositories
+            </h2>
             
-            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mt-3 rounded "
+            <button className="bg-green-500 hover:bg-green-600 text-black font-normal py-2 px-4 mt-3 rounded "
                 onClick={() => {
                     window.location.href = "http://localhost:3000/auth/github"
                 }}
