c392b50e21a1256c546aca2664d313bc98af08f9
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-01 10:39:24 -0600

    fixing some spacing in login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,20 +6,23 @@ import { FiTarget } from "react-icons/fi";
 
 export default function LoginPage() {
     return (
-        <div className="min-h-screen bg-[#0a0e14] text-white flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-semibold">
-                <span className="text-green-400">&lt;/&gt;</span>{" "}
-                <span>Code</span>
-                <span className="text-green-400">Bounty</span>
-            </h1>
-            <h2 className="mt-2 text-gray-400">
-                Peer-to-peer bug bounty platform for GitHub repositories
-            </h2>
-            
-            <div className="bg-[#151920] p-4 rounded-sm w-96 text-center mt-4">
-                <h2 className="text-lg font-semibold">Choose Your Role</h2>
+        <div className="min-h-screen bg-[#0a0e14] text-white flex flex-col items-center justify-center p-6">
+            <div className="text-center mb-8">
+                <h1 className="text-5xl font-semibold tracking-tight">
+                    <span className="text-emerald-500">&lt;/&gt;</span>{" "}
+                    <span>Code</span>
+                    <span className="text-emerald-500">Bounty</span>
+                </h1>
+                <h2 className="mt-2 text-gray-400 text-md">
+                    Peer-to-peer bug bounty platform for GitHub repositories
+                </h2>
+            </div>
 
-                <div className="bg-[#151920] p-4 rounded-lg mt-4 border-gray-700 hover:border-green-600 border-2 flex items-start gap-4">
+
+            <div className="bg-[#151920] p-8 rounded-lg w-full max-w-[420px] border border-gray-800 shadow-2xl">
+                <h2 className="text-xl font-semibold mb-2 text-center">Choose Your Role</h2>
+
+                <div className="bg-[#151920] p-4 rounded-lg mt-4 border-gray-800 hover:border-green-600 border-2 flex items-start gap-4">
                     <FiTarget color="green" size={30}/>
                     <div className="flex flex-col text-left">
                         <h2 className="font-semibold">Bug Hunter</h2>
@@ -27,7 +30,7 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-                <div className="bg-[#151920] p-4  rounded-lg mt-2 border-gray-700  hover:border-green-600 border-2 flex items-start gap-4">
+                <div className="bg-[#151920] p-4  rounded-lg mt-2 border-gray-800  hover:border-green-600 border-2 flex items-start gap-4">
                     <IoShieldOutline color="green" size={30}/>
                     <div className="flex flex-col text-left">
                         <h2 className="font-semibold">Repository Owner</h2>
@@ -35,7 +38,7 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-                <div className="bg-[#151920] p-4 rounded-lg mt-2 border-gray-700  hover:border-green-600 border-2 flex items-start gap-4">
+                <div className="bg-[#151920] p-4 rounded-lg mt-2 border-gray-800  hover:border-green-600 border-2 flex items-start gap-4">
                     <FaArrowTrendUp color="green" size={30}/>
                     <div className="flex flex-col text-left">
                         <h2 className="font-semibold">Both</h2>
@@ -46,15 +49,15 @@ export default function LoginPage() {
 
 
 
-                <button className="bg-[#10b981] hover:bg-green-600 text-black font-semibold py-2 mt-3 rounded w-full"
+                <button className="bg-emerald-500 hover:bg-emerald-400 text-black font-semibold py-2 mt-3 rounded w-full"
                                 onClick={() => {
                                     window.location.href = "http://localhost:3000/auth/github"
                                 }}
                             >
                             <FiGithub  className="mb-1 inline" />    Sign in with GitHub
                 </button>
-                <h3 className="text-xs mt-2 text-gray-400">By signing in, you agree to our
-                    <span className="underline text-gray-400 hover:text-green-400 ml-1">Terms of Service</span>
+                <h3 className="text-xs mt-2 text-gray-500 text-center">By signing in, you agree to our
+                    <span className="underline text-gray-500 hover:text-emerald-400 ml-1">Terms of Service</span>
                 </h3>
             </div>
 
