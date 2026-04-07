02aba5223b19a9fa9866f54d5f2094a5f17fb9ba
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-06 21:35:05 -0600

    implemented images/profile picture for the top three profiles

--- frontend/src/components/leaderboard/TopThreeCard.tsx
+++ frontend/src/components/leaderboard/TopThreeCard.tsx
@@ -3,14 +3,21 @@ import { FiTarget } from 'react-icons/fi'
 import { GiAchievement } from 'react-icons/gi'
 import { GoTrophy } from 'react-icons/go'
 import { FaArrowTrendUp } from "react-icons/fa6";
+import boyPic from "../../assets/profile/boy.png";
+import womanPic from "../../assets/profile/woman.png";
+import manPic from "../../assets/profile/man.png";
 
 const TopThreeCard = () => {
   return (
      <div className='flex flex-row gap-5 items-center'>
         <div 
             className='flex-1 flex flex-col gap-1 items-center justify-center text-center border border-gray-800 bg-[#151920] hover:border-emerald-700 rounded-lg p-8 '>
-            <FiTarget color="green" size={60}/>
-            <h2 className='text-6xl'>#3</h2>
+            <img
+              src={boyPic}
+              alt='profile'
+              className="w-28 h-28 rounded-full border-4 border-gray-500 object-cover"
+            />
+            <h2 className='text-6xl mt-2'>#3</h2>
             <h3 className='text-xl font-semibold text-white'>hunter_pro</h3>
         
         
@@ -34,8 +41,12 @@ const TopThreeCard = () => {
 
         <div
             className='flex-1 flex flex-col gap-1 items-center justify-center text-centerborder border-gray-800 bg-[#151920] hover:border-emerald-700 rounded-lg p-8 '>
-            <GiAchievement color="green" size={60} />
-            <h2 className='text-7xl text-emerald-500'>#1</h2>
+            <img
+              src={manPic}
+              alt='profile'
+              className="w-36 h-36 rounded-full border-4 border-emerald-500 object-cover"
+            />
+            <h2 className='text-7xl text-emerald-500 mt-2'>#1</h2>
             <h3 className='text-xl text-white font-semibold'>security_hawk</h3>
 
             <div className='flex flex-row gap-2 mt-1'>
@@ -59,8 +70,12 @@ const TopThreeCard = () => {
 
         <div
             className='flex-1 flex flex-col gap-1 items-center justify-center text-center border border-gray-800 bg-[#151920] hover:border-emerald-700 rounded-lg p-8 '>
-            <GoTrophy color="green" size={60} />
-            <h2 className='text-6xl'>#2</h2>
+            <img
+              src={womanPic}
+              alt='profile'
+              className="w-28 h-28 rounded-full border-4 border-gray-500 object-cover"
+            />
+            <h2 className='text-6xl mt-2'>#2</h2>
             <h3 className='text-xl text-white font-semibold'>bug_buster</h3>
 
             <div className='flex flex-row gap-3 mt-1'>
