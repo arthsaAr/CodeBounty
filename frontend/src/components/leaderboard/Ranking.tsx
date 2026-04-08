7d49e4b83c1ea6ca365b37933956f120ed68148a
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-08 15:40:51 -0600

    removing the header for the summary card in leaderboard
    starting the leaderboard table implementation

--- frontend/src/components/leaderboard/Ranking.tsx
+++ frontend/src/components/leaderboard/Ranking.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Ranking = () => {
   return (
-    <div>Ranking</div>
+    <div className=' bg-[#151920] border border-gray-800 rounded-lg p-3 mt-3'>
+      <h1 className='text-xl font-semibold'>All Ranking</h1>
+    </div>
   )
 }
 
