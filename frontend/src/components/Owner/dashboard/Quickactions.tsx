541b461fb04cbf669f35fbac85d58e86d9e14137
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-14 20:24:00 -0600

    fixing createBounty view in quick actions

--- frontend/src/components/Owner/dashboard/Quickactions.tsx
+++ frontend/src/components/Owner/dashboard/Quickactions.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FiTarget } from 'react-icons/fi'
 import { SlBadge } from "react-icons/sl";
+import { GrDocumentText } from "react-icons/gr";
 
 type NavBarProps = {
   setActivePage: (page: string) => void;
@@ -23,7 +24,7 @@ const Quickactions = ({ setActivePage}: NavBarProps) => {
             <div 
                 onClick={() => setActivePage("createBounty")}
                 className='flex flex-col gap-1 items-center border border-gray-800 bg-[#151920] hover:border-emerald-700 rounded-lg p-8 '>
-                <SlBadge color="green" size={55} />
+                <GrDocumentText color="green" size={55} />
                 <h2 className='text-2xl'>Create Bounty</h2>
                 <h3 className='text-lg text-gray-500'>Post a new bounty on your code</h3>
             </div>
