328653ff79b05586544eb939a73383c2a3a44f6f
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-19 23:00:00 -0600

    adding difficulty levels simple structure along with spacing adjustments

--- frontend/src/components/Owner/createBounty/subComponent/Bountyform.tsx
+++ frontend/src/components/Owner/createBounty/subComponent/Bountyform.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Bountyform = () => {
   return (
     <div className='mt-2'>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 mt-2">
           <label className="text-sm text-gray-400">File path*</label>
           <select className="bg-[#0f131a] border border-gray-800 focus:border-emerald-500 outline-none rounded-lg px-3 py-2 text-white">
             <option>src/components/Chart.tsx</option>
@@ -12,7 +12,7 @@ const Bountyform = () => {
           </select>
         </div>
 
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 mt-2">
           <label className="text-sm text-gray-400">Bounty Title*</label>
           <input
             type="text"
@@ -21,7 +21,7 @@ const Bountyform = () => {
           />
         </div>
 
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 mt-2">
           <label className="text-sm text-gray-400">Description*</label>
           <textarea
             rows={4}
@@ -30,7 +30,7 @@ const Bountyform = () => {
           />
         </div>
 
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 mt-2">
           <label className="text-sm text-gray-400">Bounty Amount (Credits)*</label>
           <input
             type="number"
@@ -38,6 +38,22 @@ const Bountyform = () => {
             className="bg-[#0f131a] border border-gray-800 focus:border-emerald-500 outline-none rounded-lg px-3 py-2 text-white placeholder-gray-500"
           />
         </div>
+
+        <div className="flex flex-col gap-1 mt-2">
+          <label className="text-sm text-gray-400">Difficulty*</label>
+          <div className='flex flex-row gap-3 '>
+            <div className='w-1/3 p-6 text-center rounded-lg border text-lg border-emerald-500'>
+              Easy
+            </div>
+            <div className='p-6 w-1/3 rounded-lg text-center border text-lg border-emerald-500'>
+              Medium
+            </div>
+            <div className='p-6 w-1/3 rounded-lg border text-center text-lg border-emerald-500'>
+              Hard
+            </div>
+          </div>
+        </div>
+
     </div>
   )
 }
