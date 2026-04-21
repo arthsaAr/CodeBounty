88786317a650c220a6c728350ae776c90b08eeda
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-21 14:39:07 -0600

    fixed the login bug, which allowed to enter dashboard even when user didn't logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,14 +27,6 @@ function App() {
           } />
       </Routes>
     </Router>
-    
-    // <div className="min-h-screen bg-black text-white flex items-center justify-center">
-    //   <h1 className="text-4xl font-bold">
-    //     <span className="text-green-400">&lt;/&gt;</span>{" "}
-    //     <span>Code</span>
-    //     <span className="text-green-400">Bounty</span>
-    //   </h1>
-    // </div>
   );
 }
 
--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,19 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navigate } from "react-router-dom";
+import axios from "axios";
+
+interface Props {
+    children: React.ReactNode;
+    allowedRole: string;
+}
 
 export default function ProtectedRoute({ children, allowedRole }: Props) {
-    const token = localStorage.getItem("token");
-    const role = localStorage.getItem("role");
 
+    const [loading, setLoading] = useState(true);
+    const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const token = localStorage.getItem("token");
+            const role = localStorage.getItem("role");
+
+            //when no token, then blocking directly
+            if(!token){
+                setLoading(false);
+                return;
+            }
+
+            if(role !== allowedRole){
+                setLoading(false);
+                return;
+            }
+
+            try{
+                await axios.get("http://localhost:3000/protected", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                setIsValid(true);
+            } catch(error){
+                //when invalid removing token and role data
+                localStorage.removeItem("token");
+                localStorage.removeItem("role");
+                setIsValid(false);
+            }
+
+            setLoading(false);
+        };
+
+        checkAuth();
+    }, [allowedRole]);
+
+    if(loading){
+        return (
+            <p>Checking access...</p>
+        );
+    }
+
+    //blocking
     //if not logged in block access
-    if(!token){
+    if(!isValid){
         return <Navigate to="/login" />;
     }
 
-    //wrong role
-    if(role !== allowedRole){
-        return <Navigate to="/login" />;
-    }
-  
     return <>{children}</>;
 }
