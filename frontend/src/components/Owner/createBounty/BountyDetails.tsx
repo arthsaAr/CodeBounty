f7d78144b7b006907c5dd1affadcb5e2fabbaf87
arthsaAr <kcraashtra431qwe@gmail.com>
2026-04-27 23:20:23 -0600

    implementing header for bounty details page

--- frontend/src/components/Owner/createBounty/BountyDetails.tsx
+++ frontend/src/components/Owner/createBounty/BountyDetails.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const BountyDetails = () => {
   return (
-    <div>BountyDetails Page | To Be Implemented</div>
+    <div>
+        <h1 className="text-3xl font-semibold mb-2 mt-3">Debounce hook optimization</h1>
+        <h3 className="text-lg mb-6 text-gray-400">The custom debounce hook could be optimized. Looking for cleaner implementation.</h3>
+    </div>
   )
 }
 
