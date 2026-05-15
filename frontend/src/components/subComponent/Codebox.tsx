9f41460f33b356372db6f7fad614ee97edc3b402
arthsaAr <kcraashtra431qwe@gmail.com>
2026-05-14 21:11:08 -0600

    rendering a simple code block in the codebox

--- frontend/src/components/subComponent/Codebox.tsx
+++ frontend/src/components/subComponent/Codebox.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Codebox = () => {
+  // Dummy code string
+  const code = `const message = "Hello World";
+console.log(message);
+
+function greet() {
+  return "This box is dummy!";
+}`;
+
   return (
-    <div className='mt-2 rounded-lg bg-[#151920] border w-full border-gray-800 p-6 justify-between items-center cursor-pointer'>
-        <h1 className='text-white text-lg'>Just a Random text for now!</h1>
+    <div className="mt-2 w-full rounded-lg border border-gray-800 bg-[#0d1117] p-6 cursor-pointer">
+      <pre className="overflow-x-auto text-sm font-mono text-gray-300">
+        <code>{code}</code>
+      </pre>
     </div>
   )
 }
