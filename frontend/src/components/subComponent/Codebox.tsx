048965886b092410a14347a9f0e15bb079b3ee1b
arthsaAr <kcraashtra431qwe@gmail.com>
2026-05-14 21:17:39 -0600

    changed the codebox into proper code box, with line numbers and file header

--- frontend/src/components/subComponent/Codebox.tsx
+++ frontend/src/components/subComponent/Codebox.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFileCode } from "react-icons/fa";
 
 const Codebox = () => {
   // Dummy code string
@@ -9,11 +10,41 @@ function greet() {
   return "This box is dummy!";
 }`;
 
+  const eachLines = code.split("\n");
+
   return (
-    <div className="mt-2 w-full rounded-lg border border-gray-800 bg-[#0d1117] p-6 cursor-pointer">
-      <pre className="overflow-x-auto text-sm font-mono text-gray-300">
-        <code>{code}</code>
-      </pre>
+    <div className="w-full h-full rounded-xl border border-[#1f2937] bg-[#0d1117] overflow-hidden">
+
+      {/* Header */}
+      <div className="flex items-center gap-2 px-3 py-3 border-b border-[#1f2937] bg-[#161b22]">
+        <FaFileCode color='green' size={20}/>
+
+        <span className="ml-1 text-sm text-gray-400 font-medium">
+          example.ts
+        </span>
+      </div>
+
+      {/* main code Area */}
+      <div className="overflow-auto h-full p-4 font-mono text-sm">
+        {eachLines.map((line, index) => (
+          <div
+            key={index}
+            className="flex gap-4 hover:bg-white/5 px-2 rounded-md"
+          >
+
+            {/* increasing line number */}
+            <span className="w-8 text-right text-gray-500 select-none">
+              {index + 1}
+            </span>
+
+            {/* main code */}
+            <span className="text-gray-300 whitespace-pre">
+              {line || " "}
+            </span>
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
