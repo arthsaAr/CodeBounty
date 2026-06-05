a82c68ed476255a12431cfe46752f8b20c8891f7
arthsaAr <kcraashtra431qwe@gmail.com>
2026-06-05 12:54:44 -0600

    added a report fetch for submissions card layout

--- frontend/src/components/subComponent/Submissions.tsx
+++ frontend/src/components/subComponent/Submissions.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type SubmissionsProps = {
   submitClicked: number;
 }
 
 const Submissions = ({ submitClicked }: SubmissionsProps) => {
+   const [recentSubmissions, setRecentSubmissions] = useState([]);
+    
+      useEffect(() => {
+          const fetchBounties = async () => {
+              try {
+              const token = localStorage.getItem("token");
+              const res = await fetch(`http://localhost:3000/bugReports/bounty/${submitClicked}`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+      
+              const data = await res.json();
+              setRecentSubmissions(data);
+              } catch (err) {
+              console.error("Failed to fetch bounties:", err);
+              }
+          };
+          
+          if (submitClicked) {
+              fetchBounties();
+          }
+          }, [submitClicked]);
+          
   return (
     <div className="w-full h-full mt-2 rounded-xl p-6 border border-[#1f2937] bg-[#0d1117] overflow-hidden">
         <h1 className='text-white font-semibold text-lg'>Submissions({submitClicked})</h1>
+
+        <div className='mt-2 rounded-lg bg-[#151920] border w-full border-gray-800 p-6 justify-between items-center cursor-pointer'>
+            TODO!
+        </div>
     </div>
   )
 }
