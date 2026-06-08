6307f150c8ed595963c07e1a94717d2e748c4b5e
arthsaAr <kcraashtra431qwe@gmail.com>
2026-06-07 20:27:20 -0600

    added card details layout for the details page
    new props are needed for lines and username & the placing of the inner sub componenets also needs to be reConsidered

--- frontend/src/components/hunter/dashboard/subcomponent/Submission.tsx
+++ frontend/src/components/hunter/dashboard/subcomponent/Submission.tsx
@@ -11,7 +11,6 @@ type SubmissionProps = {
   submissions: number;
 };
 
-
 const Submission = ({
   requestPage,
   title,
@@ -23,7 +22,21 @@ const Submission = ({
     <div className='flex gap-2 mb-3 flex-row rounded-xl bg-[#151920] border border-gray-800 p-6 justify-start items-start cursor-pointer'>
         {requestPage === "bountyDetails" ? (
           <>
-          TODO
+          <div className='bg-yellow-600/10 rounded-xl p-1'>
+              <MdAccessTimeFilled className="text-yellow-500"  opacity={0.8} size={35} />
+          </div>
+          <div>
+              <div className='flex flex-row gap-2 items-center justify-end'>
+                <h1 className='text-white text-2xl'>{title}</h1>
+                <span className='px-3 py-1 text-xs font-medium rounded-full bg-yellow-500/10 text-yellow-400 border border-yellow-500/20'>{status}</span>
+              </div>
+              <h2 className='text-gray-500 font-normal text-md'>{description}</h2>
+              <div className='flex flex-row gap-2 mt-2'>
+                  <span className='px-3 py-1 text-xs font-medium rounded-full bg-red-500/10 text-red-400 border border-red-500/20'>{difficulty}</span>
+                  <span className='text-gray-500 font-normal text-md'>Lines 46-60</span>
+                  <span className='text-gray-500 font-normal text-md'>By userX22B</span>
+              </div>
+          </div>
           </>
         ) : (
           <>
