8d56420432765118289481fac5d8c5e5875ed011
arthsaAr <kcraashtra431qwe@gmail.com>
2026-06-13 21:55:42 -0600

    fixed the dashboard recent activity card error due to commit #5df8909

--- frontend/src/components/hunter/dashboard/subcomponent/Submission.tsx
+++ frontend/src/components/hunter/dashboard/subcomponent/Submission.tsx
@@ -23,11 +23,11 @@ const Submission = ({
   lineEnd
 }: SubmissionProps) => {
   return (
-    <div className='mb-2 rounded-xl bg-[#151920] border border-gray-800 p-6 justify-start items-start cursor-pointer'>
+      <>
         {requestPage === "bountyDetails" ? (
-          <>
-          <div>
-              <div className='flex flex-row gap-2 items-center justify-between w-full'>
+          <>  
+          <div className='mb-2 rounded-xl bg-[#151920] border border-gray-800 p-6 justify-start items-start cursor-pointer'>
+              <div className='flex flex-row gap-2 items-center justify-between'>
                 <h1 className='text-white text-2xl'>{title}</h1>
                 <span className='px-3 py-1 text-xs font-medium rounded-full bg-yellow-500/10 text-yellow-400 border border-yellow-500/20'>{status}</span>
               </div>
@@ -40,21 +40,21 @@ const Submission = ({
           </div>
           </>
         ) : (
-          <>
-          <div className='bg-yellow-600/10 rounded-xl p-1'>
-              <MdAccessTimeFilled className="text-yellow-500"  opacity={0.8} size={35} />
+          <div className='flex gap-2 mb-3 flex-row rounded-xl bg-[#151920] border border-gray-800 p-6 justify-start items-start cursor-pointer'>
+            <div className='bg-yellow-600/10 rounded-xl p-1'>
+                <MdAccessTimeFilled className="text-yellow-500"  opacity={0.8} size={35} />
+            </div>
+            <div>
+                <h1 className='text-white text-2xl'>{title}</h1>
+                <h2 className='text-gray-500 font-normal text-md'>{description}</h2>
+                <div className='flex flex-row gap-2 mt-2'>
+                    <span className='px-3 py-1 text-xs font-medium rounded-full bg-red-500/10 text-red-400 border border-red-500/20'>{difficulty}</span>
+                    <span className='px-3 py-1 text-xs font-medium rounded-full bg-yellow-500/10 text-yellow-400 border border-yellow-500/20'>{status}</span>
+                </div>
+            </div>
           </div>
-          <div>
-              <h1 className='text-white text-2xl'>{title}</h1>
-              <h2 className='text-gray-500 font-normal text-md'>{description}</h2>
-              <div className='flex flex-row gap-2 mt-2'>
-                  <span className='px-3 py-1 text-xs font-medium rounded-full bg-red-500/10 text-red-400 border border-red-500/20'>{difficulty}</span>
-                  <span className='px-3 py-1 text-xs font-medium rounded-full bg-yellow-500/10 text-yellow-400 border border-yellow-500/20'>{status}</span>
-              </div>
-          </div>
-          </>
         )}
-    </div>
+      </>
   )
 }
 
